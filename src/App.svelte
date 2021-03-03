b02833f4684435c7d5c583a102ee15ff52f5fc2e
Damian Soto <soto.platero@gmail.com>
2021-03-03 14:08:40 -0500

    fix await auth

--- src/App.svelte
+++ src/App.svelte
@@ -5,21 +5,21 @@
   import ProfileConfig from "./routes/ProfileConfig.svelte";
   import ProfileShow from "./routes/ProfileShow.svelte";
   import Home from "./routes/Home.svelte";
-  import { auth } from './components/AuthUser.svelte'        
+  import {auth} from "./components/AuthUser.svelte"
 
   export let url = "";
   let userPromise = auth()
-
-  onMount(async()=>{
-    // user = await auth()
-  }) 
 
 </script>
 
 <div>
   <Router url="{url}">
     {#await userPromise}
-      loading
+      <div class="h-screen flex items-center justify-center">
+        <svg class="animate-bounce w-10 h-10 text-amber-900 text-purple-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>              
+      </div>
     {:then user} 
       <Route path="/" component="{Home}" {user} />  
       <Route path="/profile" component="{ProfileConfig}" {user} />
