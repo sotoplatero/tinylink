9d817f99973e898b2c3d3c1deeadb2e6cfe4d786
Damian Soto <soto.platero@gmail.com>
2021-03-04 16:56:32 -0500

    add notfound page

---
+++ src/components/NotFound.svelte
@@ -0,0 +1,11 @@
+<div class="h-screen flex items-center justify-center text-center">
+    <div>
+        <svg class="w-12 h-12 mx-auto text-red-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>
+        <div class="text-xl font-semibold">
+            This profile does not exist. <br>
+            <a class="text-blue-600 hover:underline" href="/profile">Do you want to create your profile?</a> 
+        </div>
+    </div>
+</div>       
--- src/routes/ProfileShow.svelte
+++ src/routes/ProfileShow.svelte
@@ -2,6 +2,7 @@
 <script>
     import Avatar from '../components/Avatar.svelte'
     import Item from '../components/Item.svelte'    
+    import NotFound from '../components/NotFound.svelte'    
 
     export let slug
 
@@ -32,7 +33,7 @@
 {#await profilePromise }
 
     <div class="h-screen flex items-center justify-center">
-        <svg class="animate-bounce w-10 h-10 text-amber-900 text-purple-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <svg class="animate-bounce w-12 h-12 text-amber-900 text-purple-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>              
     </div>
@@ -72,11 +73,7 @@
 
         {:else}
         
-            <div class="h-screen flex items-center justify-center">
-                <svg class="w-10 h-10 text-red-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
-            </div>        
+            <NotFound />
 
         {/if}
 
